a572b009de772ba5858f53b9962e1bba6748a9b4
Furiza31 <hugo.wendjaneh@gmail.com>
2024-11-08 14:54:29 +0100

    feat(show&customer): add all shows and customers

---
+++ build/CMakeFiles/JADSSHOW.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JADSSHOW.dir/main.cpp.o"
+  "CMakeFiles/JADSSHOW.dir/main.cpp.o.d"
+  "JADSSHOW"
+  "JADSSHOW.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JADSSHOW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
